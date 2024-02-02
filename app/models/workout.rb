d233ba5bc9ca1396c0a4803db296e555b990ad0e
HarrisAva <avaharris1441@gmail.com>
2024-02-02 10:54:07 -0600

    modify duration validations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,8 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, length: {minimum: 5, maximum: 50}
+  validates :name, presence: true, length: {maximum: 50}
   validates :date, presence: true
-  validates :duration, presence: true, numericality: {greater_than: 0} 
-  #{less_than: 240}
+  validates :duration, presence: true, :numericality => {:greater_than => 0}, length: { maximum: 240}
 end
