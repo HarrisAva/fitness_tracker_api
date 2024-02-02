017f953826c0e2c67354084e1dad723d907f461f
HarrisAva <avaharris1441@gmail.com>
2024-02-02 01:24:53 -0600

    complete exercises 4

---
+++ app/models/workout.rb
@@ -0,0 +1,8 @@
+class Workout < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true, length: {minimum: 5, maximum: 50}
+  validates :date, presence: true
+  validates :duration, presence: true, numericality: {greater_than: 0} 
+  #{less_than: 240}
+end
